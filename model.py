222779ab8a8c3a1200a6e743e5a48e8db276b2d9
Hailey0528 <huanhuanxiong0@gmail.com>
2018-08-16 01:07:30 +0200

    Update model.py

--- model.py
+++ model.py
@@ -9,6 +9,7 @@ import random
 import math
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
+
 # Read the columns from driving_log.csv 
 columns = ['center', 'left', 'right', 'steering', 'throttle', 'brake', 'speed']
 data = pandas.read_csv('../../ubuntu/data/driving_log.csv', skiprows=[0], names=columns)
@@ -26,7 +27,6 @@ center, X_valid, steering, y_valid = train_test_split(center, steering, test_siz
 # Divide the training data into 3 groups based on the steering angle >0.15, <-0.15, or between -0.15 and 0.15
 img_center, img_left, img_right = [], [], []
 steer_center, steer_left, steer_right = [], [], []
-
 for i in steering:
   index=steering.index(i)
   if i > 0.15:
@@ -39,7 +39,7 @@ for i in steering:
     img_center.append(center[index])
     steer_center.append(i)
 
-# add recovery
+# add recovery to make the number of straight images, left images and right images are equal
 center_size, left_size, right_size = len(img_center), len(img_left),len(img_right)
 main_size = math.ceil(len(center_recover))
 l_add = center_size-left_size
@@ -65,14 +65,7 @@ for i in indice_R:
 ## COMBINE TRAINING IMAGE NAMES AND ANGLES INTO X_train and y_train ##  
 img_train = img_center + img_left + img_right
 steering_train = np.float32(steer_center + steer_left + steer_right)
-print(img_train)
-'''Trains a simple deep NN on the MNIST dataset.
-Gets to 98.40% test accuracy after 20 epochs
-(there is *a lot* of margin for parameter tuning).
-2 seconds per epoch on a K520 GPU.
-'''
 
-from __future__ import print_function
 
 import keras
 from keras.datasets import mnist
@@ -85,39 +78,9 @@ batch_size = 128
 num_classes = 10
 epochs = 20
 
-# the data, shuffled and split between train and test sets
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-
-x_train = x_train.reshape(60000, 784)
-x_test = x_test.reshape(10000, 784)
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-x_train /= 255
-x_test /= 255
-print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
-
-# convert class vectors to binary class matrices
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
-
-model = Sequential()
-model.add(Dense(512, activation='relu', input_shape=(784,)))
-model.add(Dropout(0.2))
-model.add(Dense(512, activation='relu'))
-model.add(Dropout(0.2))
-model.add(Dense(10, activation='softmax'))
-
-model.summary()
-
-model.compile(loss='categorical_crossentropy',
-              optimizer=RMSprop(),
-              metrics=['accuracy'])
-
-history = model.fit(x_train, y_train,
-                    batch_size=batch_size,
 X_train = []
 y_train = []
+image = cv2.imread('../../ubuntu/data/IMG/'+img_train0])
 for i in range(len(img_train)):
     image = cv2.imread('../../ubuntu/data/IMG/'+img_train[i])
     #print(image.shape)    
